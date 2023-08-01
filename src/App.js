09ee526869a8f7b866d8448fbcac9a0d07b7d228
Josh Slavin <47463508+Accipitrade@users.noreply.github.com>
2023-08-01 14:44:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Circle from './Circle.js';
 import { CheckCC, MoveCC, GetCCVel, CheckCW, MoveCW, GetCWVel, GetMagnitude } from './CollisionDetection.js';
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 
 function App() {
 
@@ -83,98 +83,100 @@ function App() {
           randomPush.current = {id, velocity};
   }
 
-  const animate = () => {
-    setCircles((prevCircles) => {
-      let newCircles = [...prevCircles];
-  
-      // Calculate the maximum velocity magnitude among all circles
-      let maxVelocityMagnitude = 0;
-      prevCircles.forEach((circle) => {
-        const velocityMagnitude = GetMagnitude(circle.velocity);
-        if (velocityMagnitude > maxVelocityMagnitude) {
-          maxVelocityMagnitude = velocityMagnitude;
-        }
-      });
-  
-      // Calculate the number of steps needed for this frame
-      const steps = Math.ceil(maxVelocityMagnitude * timeStep);
-  
-      for (let step = 0; step < steps; step++) {
-        // Update positions for each time step
-        newCircles = newCircles.map((circle) => {
-          let newVelocity;
-          if(circle.id === randomPush.current.id){
-            newVelocity = {
-              x: randomPush.current.velocity.x * 0.98,
-              y: randomPush.current.velocity.y * 0.98,
-            };
-          } else {
-            newVelocity = {
-              x: circle.velocity.x * 0.98,
-              y: circle.velocity.y * 0.98,
-            };
+  const animate = useCallback(() => {
+    {
+      setCircles((prevCircles) => {
+        let newCircles = [...prevCircles];
+    
+        // Calculate the maximum velocity magnitude among all circles
+        let maxVelocityMagnitude = 0;
+        prevCircles.forEach((circle) => {
+          const velocityMagnitude = GetMagnitude(circle.velocity);
+          if (velocityMagnitude > maxVelocityMagnitude) {
+            maxVelocityMagnitude = velocityMagnitude;
           }
-          const newPosition = {
-            x: circle.position.x + newVelocity.x * timeStep,
-            y: circle.position.y + newVelocity.y * timeStep,
-          };
-  
-          // Check and resolve Circle-Wall collisions
-          if (CheckCW(newPosition, circle.radius, screenSize.current.width, screenSize.current.height)) {
-            console.log("colliding with wall!");
-            const [newPos, wallNormal] = MoveCW(newPosition, circle.radius, screenSize.current.width, screenSize.current.height);
-            newPosition.x = newPos.x;
-            newPosition.y = newPos.y;
-            newVelocity = GetCWVel(newPosition, newVelocity, wallNormal);
-          }
-  
-          return {
-            ...circle,
-            position: newPosition,
-            velocity: newVelocity,
-          };
         });
-  
-        // Check and resolve collisions after each time step
-        for (let i = 0; i < newCircles.length; i++) {
-          for (let j = i + 1; j < newCircles.length; j++) {
-            if (
-              CheckCC(
-                newCircles[i].position,
-                newCircles[i].radius,
-                newCircles[j].position,
-                newCircles[j].radius
-              )
-            ) {
-              const collisionPositions = MoveCC(
-                newCircles[i].position,
-                newCircles[i].radius,
-                newCircles[j].position,
-                newCircles[j].radius
-              );
-              const collisionVelocities = GetCCVel(
-                newCircles[i].position,
-                newCircles[i].velocity,
-                newCircles[j].position,
-                newCircles[j].velocity
-              );
-  
-              newCircles[i].position = collisionPositions[0];
-              newCircles[j].position = collisionPositions[1];
-              newCircles[i].velocity = collisionVelocities[0];
-              newCircles[j].velocity = collisionVelocities[1];
+    
+        // Calculate the number of steps needed for this frame
+        const steps = Math.ceil(maxVelocityMagnitude * timeStep);
+    
+        for (let step = 0; step < steps; step++) {
+          // Update positions for each time step
+          newCircles = newCircles.map((circle) => {
+            let newVelocity;
+            if(circle.id === randomPush.current.id){
+              newVelocity = {
+                x: randomPush.current.velocity.x * 0.98,
+                y: randomPush.current.velocity.y * 0.98,
+              };
+            } else {
+              newVelocity = {
+                x: circle.velocity.x * 0.98,
+                y: circle.velocity.y * 0.98,
+              };
+            }
+            const newPosition = {
+              x: circle.position.x + newVelocity.x * timeStep,
+              y: circle.position.y + newVelocity.y * timeStep,
+            };
+    
+            // Check and resolve Circle-Wall collisions
+            if (CheckCW(newPosition, circle.radius, screenSize.current.width, screenSize.current.height)) {
+              console.log("colliding with wall!");
+              const [newPos, wallNormal] = MoveCW(newPosition, circle.radius, screenSize.current.width, screenSize.current.height);
+              newPosition.x = newPos.x;
+              newPosition.y = newPos.y;
+              newVelocity = GetCWVel(newPosition, newVelocity, wallNormal);
+            }
+    
+            return {
+              ...circle,
+              position: newPosition,
+              velocity: newVelocity,
+            };
+          });
+    
+          // Check and resolve collisions after each time step
+          for (let i = 0; i < newCircles.length; i++) {
+            for (let j = i + 1; j < newCircles.length; j++) {
+              if (
+                CheckCC(
+                  newCircles[i].position,
+                  newCircles[i].radius,
+                  newCircles[j].position,
+                  newCircles[j].radius
+                )
+              ) {
+                const collisionPositions = MoveCC(
+                  newCircles[i].position,
+                  newCircles[i].radius,
+                  newCircles[j].position,
+                  newCircles[j].radius
+                );
+                const collisionVelocities = GetCCVel(
+                  newCircles[i].position,
+                  newCircles[i].velocity,
+                  newCircles[j].position,
+                  newCircles[j].velocity
+                );
+    
+                newCircles[i].position = collisionPositions[0];
+                newCircles[j].position = collisionPositions[1];
+                newCircles[i].velocity = collisionVelocities[0];
+                newCircles[j].velocity = collisionVelocities[1];
+              }
             }
           }
         }
-      }
-  
-      return newCircles;
-    });
-    randomPush.current = '';
-    animationFrameID.current = requestAnimationFrame(animate);
-  };
+    
+        return newCircles;
+      });
+      randomPush.current = '';
+      animationFrameID.current = requestAnimationFrame(animate);
+    };
+  });
 
-  
+
 useEffect(() => {
 
   setCircles(baseCircles);
@@ -197,7 +199,7 @@ useEffect(() => {
   return () => {
       window.removeEventListener('load', startAnimation);
   };
-}, [baseCircles, animate]);
+}, []);
 
   return (
     <div className='app-container'>
