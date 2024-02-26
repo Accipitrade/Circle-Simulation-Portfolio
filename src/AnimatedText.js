ffa21f85311f7c6cb72a33f01527c8cc236156ad
Josh Slavin <47463508+Accipitrade@users.noreply.github.com>
2024-02-25 22:07:06 -0500

    Updated resume

--- src/AnimatedText.js
+++ src/AnimatedText.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Popup from './Popup'; 
-import resume from './JS_RESUME_2023.pdf';
+import resume from './JS_RESUME_2024-1.pdf';
 import pic from './headshot.png';
 import './AnimatedText.css';
 import styled from 'styled-components';
